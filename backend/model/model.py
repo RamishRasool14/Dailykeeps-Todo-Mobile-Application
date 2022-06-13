7e6ed24baca45b4a9560fdabf46de21d81dbc192
RamishRasool14 <74363098+RamishRasool14@users.noreply.github.com>
2022-06-13 18:46:56 +0500

    New Model

--- backend/model/model.py
+++ backend/model/model.py
@@ -1,13 +1,7 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import hashlib
-from re import A
 import uuid
-import datetime
-import random
-import string
-import copy
-from collections import defaultdict
-from itertools import groupby
+from datetime import datetime, timedelta
 
 # dataclass documentation https://docs.python.org/3/library/dataclasses.html
 
@@ -20,93 +14,40 @@ def generate_hexdigest(string: str) -> str:  # Assuming string to be in utf-8 fo
 class Task:
     """A class containing data for tasks. Using dataclass because they are used for classes that primarily store data"""
 
-    id: uuid
-    creation_time: datetime
-    owner_id: int
+    id: uuid = uuid.uuid4()
+    creation_time: datetime = datetime.now()
+    due_time: datetime = None
+    owner_id: uuid = None
     description: str = ""
     done: bool = False
+
+    def __post_init__(self):
+        if self.owner_id is None:
+            raise Exception("owner id None")
+
+        if self.due_time is None:
+            self.due_time = self.creation_time + timedelta(days=1)
+        elif self.due_time <= self.creation_time:
+            raise Exception("due time cannot be before creation time")
 
 
 @dataclass
 class User:
-    """A class containing data for registered User in the system"""
+    """A class containing data for registered User and their Tasks in the system"""
 
-    id: uuid
-    first_name: str
-    last_name: str
-    password_hash: str
+    id: uuid = uuid.uuid4()
+    first_name: str = None
+    last_name: str = None
+    password_hash: str = None
+
+    def __post_init__(self):
+        if self.first_name == None:
+            raise Exception("first name not initialized")
+        elif self.last_name == None:
+            raise Exception("last name not initialized")
+        if self.password_hash is None:
+            raise Exception("password not initialized")
+        self.password_hash = generate_hexdigest(self.password_hash)
 
     def authenticate(self, password: str) -> bool:
         return generate_hexdigest(password) == self.password_hash
-
-
-@dataclass
-class App:
-    """API Interface of application with which backend communicates with the database and sends query results to frontend after requests"""
-
-    # Data Stuctures for holding user and task data
-    user_ids_to_todolist: defaultdict(list) = field(
-        default_factory=lambda: defaultdict(list)
-    )
-
-    # A key-value pair data structure in which keys are user-ids and values are to-do lists corresponding to those user-ids
-    users = []
-
-    # Methods
-
-    def register_user(
-        self, id: uuid, first_name: str, last_name: str, password_hash: str
-    ):
-        self.users.append(User(id, first_name, last_name, password_hash))
-
-    def check_user(self, authenticate_user_id: uuid):
-        for user in self.users:
-            if user.id == authenticate_user_id:
-                return True
-        return False
-
-    def get_tasks_by_day(self, owner_id: uuid):
-        tasks = self.user_ids_to_todolist[owner_id]
-        ordered_by_day = [
-            list(v) for i, v in groupby(tasks, lambda x: x.creation_time.day)
-        ]
-        print("\nPrinting Tasks in order for User {}\n".format(owner_id))
-        for day in ordered_by_day:
-            print(day)
-            print("")
-
-    def add_task(self, task: Task) -> None:
-        if self.check_user(task.owner_id):
-            self.user_ids_to_todolist[task.owner_id].append(copy.deepcopy(task))
-
-    def print_tasks(self) -> None:  # Remove Eventually
-        print(
-            "\nPrinting Tasks for Total {} Users \n".format(
-                len(self.user_ids_to_todolist)
-            )
-        )
-        for k, value in self.user_ids_to_todolist.items():
-            print(k)
-            for task in value:
-                print(task)
-            print("")
-
-    def change_order(self, new_task: Task, indx: int) -> None:
-        tasks = self.user_ids_to_todolist[new_task.owner_id]
-        task = tasks[tasks.index(new_task)]
-        tasks.remove(task)
-        tasks.insert(indx, task)
-
-    def delete_task(self, delete_task: Task) -> None:
-        tasks = self.user_ids_to_todolist[delete_task.owner_id]
-        tasks.remove(delete_task)
-
-    def edit_task(self, edited_task: Task) -> None:
-        tasks = self.user_ids_to_todolist[edited_task.owner_id]
-        indx = list(map(lambda x: x.id, tasks)).index(edited_task.id)
-        tasks[indx] = edited_task
-
-    def mark(self, change_task: Task, mark: bool = True) -> None:
-        tasks = self.user_ids_to_todolist[change_task.owner_id]
-        indx = list(map(lambda x: x.id, tasks)).index(change_task.id)
-        tasks[indx].done = mark
--- backend/model/model_test.py
+++ backend/model/model_test.py
@@ -4,228 +4,88 @@ import uuid
 import datetime
 import random
 import string
-import copy
-from collections import defaultdict
 
 
-def create_random_task(owner_id: uuid):
-    length = 10
-    return model.Task(
-        uuid.uuid4(),
-        datetime.datetime.now(),
-        owner_id,
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=length)),
-    )
-
-
-def create_random_user():
-    length = 6
-    return model.User(
-        uuid.uuid4(),
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=length)),
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=length)),
-        model.generate_hexdigest(
-            "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
-        ),
-    )
-
-
-def test_add_user():
-    app = model.App()
-    users = [create_random_user() for x in range(10)]
-
-    for user in users:
-        app.register_user(user.id, user.first_name, user.last_name, user.password_hash)
-
-    for user in users:
-        assert app.check_user(user.id)
-
-
-def test_add_task():  # Creates random 10 tasks and tests if they are correctly added to the system
-    app = model.App()
-    tasks = []
-    test_dict = defaultdict(list)
-    for x in range(10):
-        if x < 5:
-            task = create_random_task(0)
-        else:
-            task = create_random_task(1)
-        user = create_random_user()
-        app.register_user(
-            task.owner_id, user.first_name, user.last_name, user.password_hash
-        )
-        app.add_task(task)
-        test_dict[task.owner_id].append(copy.deepcopy(task))
-
-    assert test_dict == app.user_ids_to_todolist
-
-
-def test_change_order():  # Creates random 10 tasks and change order of the first and last task
-    app = model.App()
-    tasks = []
-    test_dict = defaultdict(list)
-    for x in range(10):
-        if x < 5:
-            task = create_random_task(0)
-        else:
-            task = create_random_task(1)
-        user = create_random_user()
-        app.register_user(
-            task.owner_id, user.first_name, user.last_name, user.password_hash
-        )
-        app.add_task(task)
-        tasks.append(task)
-        test_dict[task.owner_id].append(copy.deepcopy(task))
-
-    ind1 = 0
-    ind2 = 4
-
-    test_dict[0].remove(tasks[4])
-    test_dict[0].insert(ind1, tasks[4])
-
-    app.change_order(tasks[ind2], ind1)
-    assert test_dict == app.user_ids_to_todolist
-
-
-def test_delete_task():
-    app = model.App()
-    tasks = []
-    test_dict = defaultdict(list)
-    for x in range(10):
-        if x < 5:
-            task = create_random_task(0)
-        else:
-            task = create_random_task(1)
-        user = create_random_user()
-        app.register_user(
-            task.owner_id, user.first_name, user.last_name, user.password_hash
-        )
-        app.add_task(task)
-        tasks.append(task)
-        test_dict[task.owner_id].append(copy.deepcopy(task))
-
-    app.delete_task(tasks[3])
-    test_dict[0].remove(tasks[3])
-
-    assert test_dict == app.user_ids_to_todolist
-
-
-def test_edit_task():
-    app = model.App()
-    tasks = []
-    test_dict = defaultdict(list)
-    for x in range(10):
-        if x < 5:
-            task = create_random_task(0)
-        else:
-            task = create_random_task(1)
-        user = create_random_user()
-        app.register_user(
-            task.owner_id, user.first_name, user.last_name, user.password_hash
-        )
-        app.add_task(task)
-        tasks.append(task)
-        test_dict[task.owner_id].append(copy.deepcopy(task))
-
-    tasks[3].description = "Please edit the task Bro"
-    app.edit_task(tasks[3])
-    test_dict[0][3].description = "Please edit the task Bro"
-
-    assert test_dict == app.user_ids_to_todolist
-
-
-def test_mark_complete():
-    app = model.App()
-    tasks = []
-    test_dict = defaultdict(list)
-    for x in range(10):
-        if x < 5:
-            task = create_random_task(0)
-        else:
-            task = create_random_task(1)
-        user = create_random_user()
-        app.register_user(
-            task.owner_id, user.first_name, user.last_name, user.password_hash
-        )
-        app.add_task(task)
-        tasks.append(task)
-        test_dict[task.owner_id].append(copy.deepcopy(task))
-
-    tasks[3].done = True
-    app.mark(tasks[3])
-    test_dict[0][3].done = True
-    assert test_dict == app.user_ids_to_todolist
-
-    app.mark(tasks[3], False)
-    test_dict[0][3].done = False
-    assert test_dict == app.user_ids_to_todolist
-
-
-def test_get_tasks_by_day():  # Creates a user with owner_id 1 and adds 2 tasks dated 2020/5/17, 3 tasks dated 2020/5/10 and 1 task dated 2020/5/16. Checks the method of get_tasks_by_day to see if it prints tasks correctly which it does
-    app = model.App()
-    tasks = []
-    test_dict = defaultdict(list)
+def test_create_task():
+    test_id = uuid.uuid4()
+    creation_time = datetime.datetime.now()
+    due_time = datetime.datetime(2022, 12, 12)
+    owner_id = uuid.uuid4()
+    description = "random description"
+    done = False
 
     task = model.Task(
-        uuid.uuid4(),
-        datetime.datetime(2020, 5, 17),
-        1,
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=10)),
+        id=test_id,
+        creation_time=creation_time,
+        due_time=due_time,
+        owner_id=owner_id,
+        description=description,
+        done=done,
     )
-    user = create_random_user()
-    user.id = task.owner_id
-    app.register_user(user.id, user.first_name, user.last_name, user.password_hash)
-    tasks.append(task)
-    app.add_task(task)
-    test_dict[task.owner_id].append(copy.deepcopy(task))
+    assert task.id == test_id
+    assert task.creation_time == creation_time
+    assert task.due_time == due_time
+    assert task.owner_id == owner_id
+    assert task.description == description
+    assert task.done == done
 
-    task = model.Task(
-        uuid.uuid4(),
-        datetime.datetime(2020, 5, 17),
-        1,
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=10)),
-    )
-    tasks.append(task)
-    app.add_task(task)
-    test_dict[task.owner_id].append(copy.deepcopy(task))
 
-    task = model.Task(
-        uuid.uuid4(),
-        datetime.datetime(2020, 5, 16),
-        1,
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=10)),
+def test_create_task_without_duedate():
+    task = model.Task(owner_id=uuid.uuid4())
+    assert (
+        task.creation_time.date() + datetime.timedelta(days=1) == task.due_time.date()
     )
-    tasks.append(task)
-    app.add_task(task)
-    test_dict[task.owner_id].append(copy.deepcopy(task))
 
-    task = model.Task(
-        uuid.uuid4(),
-        datetime.datetime(2020, 5, 10),
-        1,
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=10)),
-    )
-    tasks.append(task)
-    app.add_task(task)
-    test_dict[task.owner_id].append(copy.deepcopy(task))
 
-    task = model.Task(
-        uuid.uuid4(),
-        datetime.datetime(2020, 5, 10),
-        1,
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=10)),
-    )
-    tasks.append(task)
-    app.add_task(task)
-    test_dict[task.owner_id].append(copy.deepcopy(task))
-    task = model.Task(
-        uuid.uuid4(),
-        datetime.datetime(2020, 5, 10),
-        1,
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=10)),
-    )
-    tasks.append(task)
-    app.add_task(task)
-    test_dict[task.owner_id].append(copy.deepcopy(task))
+def test_create_task_with_duedate():
+    task = model.Task(owner_id=uuid.uuid4(), due_time=datetime.datetime(2025, 10, 2))
+    assert task.due_time == datetime.datetime(2025, 10, 2)
 
-    app.get_tasks_by_day(1)
+
+def test_no_ownerid():
+    with pytest.raises(Exception):
+        task = model.Task()
+
+
+def test_incorrect_due_time():
+    with pytest.raises(Exception):
+        task = model.Task(due_time=datetime.datetime(1988, 10, 2))
+
+
+def test_create_user():
+    user_id = uuid.uuid4()
+    first_name = "random first name"
+    last_name = "random last name"
+    password_hash = model.generate_hexdigest("random password")
+    user = model.User(
+        id=user_id,
+        first_name=first_name,
+        last_name=last_name,
+        password_hash="random password",
+    )
+    assert user.id == user_id
+    assert user.first_name == user.first_name
+    assert user.last_name == user.last_name
+    assert user.password_hash == password_hash
+
+
+def test_no_first_name():
+    with pytest.raises(Exception):
+        model.User()
+
+
+def test_no_last_name():
+    with pytest.raises(Exception):
+        model.User(first_name="random")
+
+
+def test_no_password():
+    with pytest.raises(Exception):
+        model.User(first_name="random first name", last_name="random last name")
+
+
+def test_autheticate_password():
+    test_pass = "correct password"
+    user = model.User(first_name="ramish", last_name="rasool", password_hash=test_pass)
+    assert user.authenticate("correct password")
+    assert not user.authenticate("incorrect password")
